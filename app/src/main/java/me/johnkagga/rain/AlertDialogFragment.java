f7498d6432d116eb0b1f89760f77409d5e02e21b
jokamjohn <johnkagga@gmail.com>
2015-06-16 14:13:11 +0300

    Dialog class refactored

--- app/src/main/java/me/johnkagga/rain/AlertDialogFragment.java
+++ app/src/main/java/me/johnkagga/rain/AlertDialogFragment.java
@@ -14,10 +14,10 @@ public class AlertDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Context context = getActivity();
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("Oops, sorry")
-                .setMessage("Please try again")
-                .setPositiveButton("Ok",null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
 
         AlertDialog dialog = builder.create();
 
