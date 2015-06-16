54075b9dd7414aa1a40984e833b706c820fce443
jokamjohn <johnkagga@gmail.com>
2015-06-16 14:01:22 +0300

    Added a okhttp call back and new method

--- app/src/main/java/me/johnkagga/rain/MainActivityFragment.java
+++ app/src/main/java/me/johnkagga/rain/MainActivityFragment.java
@@ -1,13 +1,14 @@
 package me.johnkagga.rain;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -20,7 +21,7 @@ import java.io.IOException;
  */
 public class MainActivityFragment extends Fragment {
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String TAG = "my app";
 
     public MainActivityFragment() {
     }
@@ -45,18 +46,34 @@ public class MainActivityFragment extends Fragment {
                 .url(forecastUrl)
                 .build();
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()){
-                //logging the whole response body
-                Log.v(TAG,response.body().string());
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ",e);
-        }
 
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    Log.d(TAG, response.body().string());
+                    if (response.isSuccessful()){
+                        //logging the whole response body
+                        alertUserAboutError();
+                    }
+                    else {
 
+                    }
+                } catch (IOException e) {
+                    Log.v(TAG, "Exception caught: ",e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main thread is working");
+
+    }
+
+    private void alertUserAboutError() {
     }
 
     @Override
