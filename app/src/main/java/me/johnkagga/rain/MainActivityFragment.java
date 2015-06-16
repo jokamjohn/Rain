11f384d29948f385320539d76f902847b0b07531
jokamjohn <johnkagga@gmail.com>
2015-06-16 12:46:06 +0300

    Added url

--- app/src/main/java/me/johnkagga/rain/MainActivityFragment.java
+++ app/src/main/java/me/johnkagga/rain/MainActivityFragment.java
@@ -16,8 +16,23 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String apiKey = "5a20df57556bd8bcb4924c5a083bc653";
+        double latitude = 37.8267;
+        double longitude =-122.423;
+
+        String forecastUrl = "https://api.forecast.io/forecast/"+ apiKey +"/ "
+                + latitude + "," + longitude;
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+
+
     }
 }
